30fa342a0e7c294096719f310949f3479490118f
Tom Hart <48930492+s3kay@users.noreply.github.com>
2019-03-27 22:30:51 +0000

    Add files via upload

---
+++ image_uploading.py
@@ -0,0 +1,30 @@
+import csv
+import os
+import shutil
+import glob
+
+#read in refs from csv
+with open('refs.csv', 'r') as f:
+    reader = csv.reader(f)
+    refs = list(reader)
+
+
+#set working dir and look for image
+os.chdir('F:\\python\\work_scripts')
+fname = glob.glob('*.jpg') or glob.glob('*.jpeg')
+
+
+#define source and destination for renaming and duplicating
+src = 'F:\\python\\work_scripts\\' + "%s".join(fname)
+x = 0
+
+
+
+
+#perform rename
+i = 0
+while i < len(refs):
+    shutil.copy(src, 'F:\\python\\work_scripts\\' + "%s".join(refs[x]) + ".jpg")
+    x += 1
+    i += 1
+
