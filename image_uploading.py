c1a6a8f4b8b8ace65e324852f06b9aa8be24f06b
Tom Hart <48930492+s3kay@users.noreply.github.com>
2019-03-27 22:42:00 +0000

    Update image_uploading.py

--- image_uploading.py
+++ image_uploading.py
@@ -14,17 +14,13 @@ os.chdir('F:\\python\\work_scripts')
 fname = glob.glob('*.jpg') or glob.glob('*.jpeg')
 
 
-#define source and destination for renaming and duplicating
+#define source and vars for renaming and duplicating
 src = 'F:\\python\\work_scripts\\' + "%s".join(fname)
 x = 0
-
-
-
-
-#perform rename
 i = 0
+
+#perform rename 
 while i < len(refs):
     shutil.copy(src, 'F:\\python\\work_scripts\\' + "%s".join(refs[x]) + ".jpg")
     x += 1
     i += 1
-
