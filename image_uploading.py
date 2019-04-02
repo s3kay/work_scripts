4b21d8e2ded267edde11d64f79e37defe661ce7e
Tom Hart <48930492+s3kay@users.noreply.github.com>
2019-04-02 19:30:39 +0100

    Update image_uploading.py

--- image_uploading.py
+++ image_uploading.py
@@ -7,21 +7,19 @@ import glob
 with open('refs.csv', 'r') as f:
     reader = csv.reader(f)
     refs = list(reader)
-    
-    
+
+
 #set working dir and look for image
 os.chdir('F:\\python\\work_scripts')
 fname = glob.glob('*.jpg') or glob.glob('*.jpeg')
 
 
-#define source and vars for renaming and duplicating
-src = 'F:\\python\\work_scripts\\' + "%s".join(fname)
+#define var for counting and source directory
 x = 0
-i = 0
+src = 'F:\\python\\work_scripts\\' + fname[x]
 
 
-#perform rename 
-while i < len(refs):
+#loop over list of refs creating versions of image with refs as filenames 
+for i in refs:
     shutil.copy(src, 'F:\\python\\work_scripts\\' + "%s".join(refs[x]) + ".jpg")
     x += 1
-    i += 1
